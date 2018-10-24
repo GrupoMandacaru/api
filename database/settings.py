6aa48f8d0e88b50ee84321f8813a5c36a98e1692
Anna Gabriela <agmc0997@gmail.com>
2018-10-24 15:38:20 -0300

    add .env

--- database/settings.py
+++ database/settings.py
@@ -1,5 +1,8 @@
+import os
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://admin:Admin123@ecodomo-shard-00-00-3mokt.mongodb.net:27017,ecodomo-shard-00-01-3mokt.mongodb.net:27017,ecodomo-shard-00-02-3mokt.mongodb.net:27017/test?ssl=true&replicaSet=Ecodomo-shard-0&authSource=admin&retryWrites=true')
+MONGO_URL = os.environ.get("MONGO_URL")
+
+client = MongoClient(MONGO_URL)
 
 db = client.ecodomo
