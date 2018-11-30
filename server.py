b38c6e2b0ed47e51193fbcd238cef315c421b055
Anna Gabriela <agmc0997@gmail.com>
2018-11-30 16:42:21 -0300

    add cors

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from database.queries import *
 from flask import Flask, render_template, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
